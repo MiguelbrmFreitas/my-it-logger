068e5cb1fcaf3bca568fc2fe1be730b07b9c1d83
miguelfreitas18 <miguelfreitas8000@gmail.com>
2020-08-09 00:14:06 -0300

    Logs and LogItem components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
@@ -10,9 +12,12 @@ const App = () => {
   })
 
   return (
-    <div className="App">
-      IT Logger
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
