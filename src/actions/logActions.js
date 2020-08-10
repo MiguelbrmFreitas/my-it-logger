09a30e052eb7cdfdb8924ed6890fb5275c43a32c
miguelfreitas18 <miguelfreitas8000@gmail.com>
2020-08-10 01:17:18 -0300

    Search for logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
     DELETE_LOG,
     SET_CURRENT,
     CLEAR_CURRENT,
-    UPDATE_LOG
+    UPDATE_LOG,
+    SEARCH_LOGS
 } from './types';
 
 // export const getLogs = async dispatch => {
@@ -130,6 +131,28 @@ export const updateLog = log => {
       }
     };
 };
+
+// Search for logs
+export const searchLogs = (text) => {
+    return async dispatch => {
+      try {
+          setLoading();
+  
+          const res = await fetch(`/logs?q=${text}`);
+          const data = await res.json();
+  
+          dispatch({
+              type: SEARCH_LOGS,
+              payload: data
+          })
+      } catch (err) {
+          dispatch({
+              type: LOGS_ERROR,
+              payload: err.response.data
+          })
+      }
+    };
+  };
 
 // Set current log
 export const setCurrent = log => {
